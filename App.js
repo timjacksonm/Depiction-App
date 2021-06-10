80eed3c6a8e2e0bafd93076c7bec08c4aa00003c
Tim Jackson <timjacksonm@gmail.com>
2021-06-10 15:03:26 -0500

    Login Screen Updates -added background Image. -added fonts to project. -asynchronous load fonts before login screen is displayed.

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import Login from './src/components/Screens/Login';
 import Home from './src/components/Screens/Home';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
+const getFonts = () => Font.loadAsync({
+  playfairRegular: require('./src/assets/fonts/PlayfairDisplay-Regular.ttf'),
+  playfairItalic: require('./src/assets/fonts/PlayfairDisplay-Italic.ttf'),
+  playfairSemiBold: require('./src/assets/fonts/PlayfairDisplay-SemiBold.ttf'),
+  playfairBold: require('./src/assets/fonts/PlayfairDisplay-Bold.ttf'),
+  playfairExtraBold: require('./src/assets/fonts/PlayfairDisplay-ExtraBold.ttf'),
+});
 
 export default function App() {
+  const [authPass, setAuthPass] = useState(false);
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  const [authPass, setAuthPass] = useState(true);
-
-  const changeAuth = () => {
-    setAuthPass(!authPass)
+  if (!fontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => console.log('error')}
+    />
+    )
+  } else {
+    return (
+      <View style={{flex: 1}}>
+        {authPass ? <Home /> : <Login setAuthPass={setAuthPass}/>}
+        <StatusBar style="light" />
+      </View>
+      )
   }
-
-  return (
-    <View style={{ height: '100%' }}>
-      {authPass ? <Home /> : <Login />}
-      <Button
-      onPress={changeAuth}
-      title='Test'
-      />
-      <StatusBar style="auto" />
-    </View>
-  )
 }
--- src/components/Screens/Login.js
+++ src/components/Screens/Login.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ImageBackground } from 'react-native';
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
+      <ImageBackground source={require('../../assets/background.jpg')} style={styles.image}>
+      <Text style={styles.title}>Depiction</Text>
+      <View>
+        <TextInput></TextInput>
+      </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -12,8 +17,17 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  title: {
+    fontFamily: 'playfairExtraBold',
+    padding: 50,
+    color: 'white',
+    fontSize: 44,
+    textAlign: 'center',
+  }
 });
