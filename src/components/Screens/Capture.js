4ca4f6cd783e9e1e70960312f664f0ffe929af13
Tim Jackson <timjacksonm@gmail.com>
2021-06-10 21:35:39 -0500

    Gallery page navigation update -added camera icon that has an onPress listener. When pressed navigates to Capture Screen note couldnt find a way to store the component while still being able to use the navigate prop. So I had to include the component for the icon inline inside the Home function. Not ideal.

--- src/components/Screens/Capture.js
+++ src/components/Screens/Capture.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Login() {
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
         <Text>Capture Screen</Text>
       </View>
     );
--- src/components/Screens/Home.js
+++ src/components/Screens/Home.js
@@ -1,26 +1,48 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import CaptureScreen from './Capture';
+import { Entypo } from '@expo/vector-icons'; 
 
-function GalleryScreen() {
+function GalleryScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>GalleryScreen</Text>
+      <Text>Gallery of pictures goes here</Text>
     </View>
   );
 }
 
 const Stack = createStackNavigator();
 
-function Home() {
+export default function Home() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Gallery" component={GalleryScreen} />
+      <Stack.Navigator initialRouteName="Gallery">
+        <Stack.Screen
+          name="Gallery"
+          component={GalleryScreen}
+          options={
+            ({ navigation }) => ({
+              title: 'Gallery',
+              headerStyle: {
+                backgroundColor: '#588467',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontFamily: 'playfairBold',
+              },
+              // Had to collapse this out instead of storing in a variable. Adds Camera to header with onPress() sends to Capture Screen.
+              headerRight: () => 
+              (
+              <View style={{marginRight: 20}}>
+                <Entypo name="camera" size={28} color="#fff" onPress={() => navigation.navigate('Capture')} />
+              </View>
+              )
+              })}
+              headerStyle={{backgroundColor: 'blue'}}/>
+        <Stack.Screen name="Capture" component={CaptureScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-export default Home;
